c1384fbde5cafbea662fc96eff696a1c716dda5a
GeandroMarques <marquess.2608@gmail.com>
2025-10-17 19:41:33 -0300

    Classe No e pilha finalizadas

--- src/No.java
+++ src/No.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
-
 public class No {
+    private Elemento elemento;
+    private No proximo;
+
+    public No (Elemento elemento) {
+        this.elemento = elemento;
+        this.proximo = null;
+    }
+
+    public Elemento getElemento() {
+        return elemento;
+    }
+
+    public void setElemento(Elemento elemento) {
+        this.elemento = elemento;
+    }
+
+    public No getProximo() {
+        return proximo;
+    }
+
+    public void setProximo(No proximo) {
+        this.proximo = proximo;
+    }
 }
--- src/Pilha.java
+++ src/Pilha.java
@@ -1,4 +1,43 @@
-package PACKAGE_NAME;
-
 public class Pilha {
+    private No topo;
+
+    public Pilha () {
+        this.topo = null;
+    }
+
+    public boolean vazia() {
+        return topo == null;
+    }
+
+    public void inserirTopo(Elemento elemento) {
+        No no = new No(elemento);
+        if(vazia()) {
+            topo = no;
+        } else {
+            no.setProximo(topo);
+            topo = no;
+        }
+    }
+
+    public void removerTopo() {
+        if(vazia()) {
+            System.out.println("Histórico de solicitações vazio!");
+        } else {
+            topo = topo.getProximo();
+        }
+    }
+
+    public void verificarHistorico() {
+        if (vazia()) {
+            System.out.println("Histórico de solicitações vazio!");
+        } else {
+            No atual = topo;
+            while(atual != null) {
+                System.out.println("ID da solicitação: " + atual.getElemento().getId());
+                System.out.println("Descrição: " + atual.getElemento().getDescricao());
+                System.out.println("Data e hora: " + atual.getElemento().getData_hora());
+                atual = atual.getProximo();
+            }
+        }
+    }
 }
