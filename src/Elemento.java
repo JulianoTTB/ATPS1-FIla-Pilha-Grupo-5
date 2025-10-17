c0da28d60f77eefefdfbc1150fc5df2326fb2aca
GeandroMarques <marquess.2608@gmail.com>
2025-10-17 19:40:52 -0300

    classe elemento finalizada

--- src/Elemento.java
+++ src/Elemento.java
@@ -1,4 +1,35 @@
-package PACKAGE_NAME;
-
 public class Elemento {
+    private String id;
+    private String descricao;
+    private String data_hora;
+
+    public Elemento(String id, String descricao, String data_hora) {
+        this.id = id;
+        this.descricao = descricao;
+        this.data_hora = data_hora;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getData_hora() {
+        return data_hora;
+    }
+
+    public void setData_hora(String data_hora) {
+        this.data_hora = data_hora;
+    }
 }
